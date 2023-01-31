3e2cdf1e7525545beeeb529b80a04ee2ca943e0a
HiroshiSukehiro <niklowh@gmail.com>
2023-01-31 16:57:59 +0300

    auth update

--- src/common/midleware/auth.middleware.ts
+++ src/common/midleware/auth.middleware.ts
@@ -7,12 +7,11 @@ export const AuthMiddleware: FieldMiddleware = async (
   next: NextFn,
 ) => {
         const auth = ctx.context.req.cookies['authorization'] || ctx.context.req.headers.authorization;
-        let prismaService = new PrismaService();
+        const prismaService = new PrismaService();
         if (!!auth) {
-            const decoded: any = jwtDecode(auth)
-            let email = decoded.email
+            const { email }: any = jwtDecode(auth)
             if (!!email) {
-                let user = await prismaService.user.findUnique({where: {email}});
+                const user = await prismaService.user.findUnique({where: {email}});
                 if (!!user) {
                     ctx.context.req.user = user;
                 }
@@ -20,5 +19,4 @@ export const AuthMiddleware: FieldMiddleware = async (
             } 
         }
         return next();
-
 };
