72270ad2860be5422af37d01e621b088f4ab04e7
unknown <stanislavmorr@gmail.com>
2023-02-02 18:25:13 +0300

    Правка в статистике

--- src/core/statistic/resolvers/statistic-query.resolver.ts
+++ src/core/statistic/resolvers/statistic-query.resolver.ts
@@ -1,4 +1,5 @@
 import { Args, ResolveField, Resolver } from '@nestjs/graphql';
+import { CheckAuthMiddleware } from '../../../common/midleware';
 import { StatisticService } from '../services/statistic.service';
 import { GetStatisticResultType } from "../models/results/get-statistic-user-result"
 import { StatisticQueryType, StatisticRootResolver } from "./statistic-root.resolver"
@@ -8,7 +9,7 @@ import { GetStatisticInputType } from "../models/inputs/get-statistic-user-input
 export class StatisticQueryResolver {
     constructor(private readonly StatisticService: StatisticService) { }
 
-    @ResolveField(() => GetStatisticResultType)
+    @ResolveField(() => GetStatisticResultType, { middleware: [CheckAuthMiddleware] })
     async getStatistic(@Args() input: GetStatisticInputType): Promise<GetStatisticResultType> {
         return this.StatisticService.getStatisticByUsers(input);
     }
--- src/core/statistic/resolvers/statistic-root.resolver.ts
+++ src/core/statistic/resolvers/statistic-root.resolver.ts
@@ -4,14 +4,6 @@ import {
     GetStatisticResultType,
 } from '../models/results/get-statistic-user-result';
 
-@ObjectType()
-export class StatisticMutationType {
-
-    @Field(() => GetStatisticResultType, {
-        description: 'Delete Statistic',
-    })
-    deleteStatistic: GetStatisticResultType;
-}
 
 @ObjectType()
 export class StatisticQueryType {
@@ -24,12 +16,6 @@ export class StatisticQueryType {
 
 @Resolver()
 export class StatisticRootResolver {
-    @Mutation(() => StatisticMutationType, {
-        description: 'Statistic mutations',
-    })
-    StatisticMutations() {
-        return {};
-    }
 
     @Query(() => StatisticQueryType, {
         description: 'Statistic queries'
